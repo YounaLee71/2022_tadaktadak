3206e1a01ee1c740ff9c348b8ec9258ed237c7fd
YounaLee71 <leeyouna9898@gmail.com>
2022-04-10 23:21:30 +0900

    2nd

---
+++ app/src/main/java/com/example/tadaktadak/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.tadaktadak;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 5000); //2초동안 화면 띄우기
+    }
+}
